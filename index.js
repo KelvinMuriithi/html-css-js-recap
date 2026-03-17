77b3251465036b695f15c6107d28a7017e6d3ea5
Kelvinm <kelvinmurithi18@gmail.com>
2026-03-17 10:54:24 +0300

    Added webpages

---
+++ index.js
@@ -0,0 +1,10 @@
+const loc = "Ngong road, Nairobi, Kenya";
+//  loc ="Kiambu road, Nairobi, Kenya";
+let age = 25;
+age = 30;
+var studentName = "John Doe";
+studentName = "Jane Doe";
+
+// console.log(loc)
+// console.log(age)
+console.log(studentName)
